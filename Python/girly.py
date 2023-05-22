7d838c549353a0558ad3ca4020bf732ec6f66313
sseezzuu <sebas.zuga@gmail.com>
2023-05-22 17:13:10 -0500

    Se agrega archivo girly.py

---
+++ Python/girly.py
@@ -0,0 +1,7 @@
+girly=input("¿Girly me va a dar postre?, DIGA SI O NO").lower()
+if girly == "no":
+    print("Sacar a girly de la casa")
+elif girly=="si":
+    print("Darle un beso")
+else:
+    print("responda bien pendeja")
